52efbfe2e197224a1b6620f5e91de25499426e5b
verteramofederico <verteramo.federico@gmail.com>
2022-08-27 22:05:40 +0200

    about us and other fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import './App.css';
-
 import Root from './routes/root'
 
 function App() {
--- src/pages/NosotrosPage.tsx
+++ src/pages/NosotrosPage.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 const NosotrosPage = () => {
     return (
-        <div>Nosotros ok</div>
+        <section className="relative pt-24 pb-32 overflow-hidden">
+            <div className="container mx-auto px-4">
+                <h2 className="mb-5 max-w-2xl mx-auto font-heading font-bold text-center text-5xl sm:text-5xl text-gray-900">
+                    Nuestra historia y presente
+                </h2>
+                <p className="mt-5 text-base max-w-2xl mx-auto text-gray-600">
+                Restaurant FV nace en Roma, Italia. A mediados del siglo XX, una familia funda un humilde Restaurante en el tradicional Barrio Trastévere.
+                </p>
+                <p className="mt-5 text-base max-w-2xl mx-auto text-center text-gray-600">
+                Con más de 29 años de presencia en Argentina, Restuarant FV sigue siendo fiel al estilo que le dio origen: calidad, cordialidad y una esmerada atención; manteniendo intacta la esencia a lo largo de su historia y logrando distinguir el verdadero sabor de la cocina italiana, FV se ha convertido en un clásico de Buenos Aires.
+                </p>
+                <p className="mt-5 text-base max-w-2xl mx-auto text-center text-gray-600">
+                Dentro de su completa carta, donde las pastas son protagonistas por excelencia, encontrará una amplia variedad de opciones apta para los paladares más exigentes.
+                </p>
+            </div>
+    </section>
     )
 }
 
