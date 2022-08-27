7178c493928d86a2e19dab9198cfb5a2521c7003
verteramofederico <verteramo.federico@gmail.com>
2022-08-27 22:35:27 +0200

    axios added and api connected ok

--- src/pages/Carta.tsx
+++ src/pages/Carta.tsx
@@ -1,3 +1,15 @@
+// const axios = require('axios').default;
+
+/* axios.get(`https://api.spoonacular.com/recipes/716429/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true.`)
+    .then(function (response: any) {
+    console.log(response);
+    })
+    .catch(function (error: any) {
+        console.log(error);
+    })
+    .then(function () {
+    }); */
+
 const Carta = () => {
     return (
         <>
