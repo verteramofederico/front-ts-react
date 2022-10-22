64ece0f14765391640df642b167a1c827f4e4225
verteramofederico <verteramo.federico@gmail.com>
2022-10-22 22:44:07 +0200

    .gitignore is now working

--- src/pages/Carta.tsx
+++ src/pages/Carta.tsx
@@ -1,20 +1,32 @@
-// const axios = require('axios').default;
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+const axios = require('axios').default;
 
-/* axios.get(`https://api.spoonacular.com/recipes/716429/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true.`)
-    .then(function (response: any) {
-    console.log(response);
-    })
-    .catch(function (error: any) {
-        console.log(error);
-    })
-    .then(function () {
-    }); */
+interface UserType {
+    user: {
+        email: string;
+        fullName: string;
+        token: string
+    };
+} 
 
 const Carta = () => {
+    const { email } = useSelector((state: UserType) => state.user)
+    const [dishTypes, setDishTypes] = useState(["2", "6", "4"]);
+
+    useEffect(() => {
+        axios
+        //.get(`https://api.spoonacular.com/recipes/complexSearch?type=maincourse?apiKey=${process.env.REACT_APP_API_KEY}`)
+        .get(`https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=2?apiKey=fee5ec9fa73f41678b8517cc11bd55f4`)
+        .then ((response: { data: any, dishTypes: any }) => setDishTypes(response.data.dishTypes));
+    },[]); 
+
     return (
         <>
+        <h1>Bienvenido {email}</h1>
         <div>Carta</div>
-        {console.log(process.env.REACT_APP_TEST)}
+        { dishTypes.length >= 1? dishTypes.map((dish: any) => { return console.log(dish)})
+        :null}
         </>
         
     )
